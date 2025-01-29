396f1a90a8e82c9f6b2871310e3f6c930c354f2f
Extager <164411975+Extager@users.noreply.github.com>
2025-01-29 23:19:20 +0300

    debugging-savePDF

--- src/shared/ui/CV/UICV.tsx
+++ src/shared/ui/CV/UICV.tsx
@@ -1,29 +1,38 @@
-import { forwardRef, useRef } from "react";
+import { useRef } from "react";
 import { observer } from "mobx-react-lite";
 import ResumeStore from "../../../store/ResumeStore";
 import { useReactToPrint } from "react-to-print";
+import './UICV.css';
 
 const UICV = () => {
   const contentRef = useRef<HTMLDivElement>(null);
-  const contentSave = useReactToPrint({contentRef});
+
+  const contentSave = useReactToPrint({
+    contentRef,
+    documentTitle: "YourCV",
+    bodyClass: "print-style",
+  });
   return (
-    <div ref={contentRef}>
-      <h2>{ResumeStore.title}</h2>
-      <p>
-        <strong>Name:</strong> {ResumeStore.firstName} {ResumeStore.lastName}
-      </p>
-      <p>
-        <strong>Email:</strong> {ResumeStore.email}
-      </p>
-      <p>
-        <strong>Phone:</strong> {ResumeStore.phoneNumber}
-      </p>
-      <p>
-        <strong>Address:</strong> {ResumeStore.address}
-      </p>
-      <p>
-        <strong>Description:</strong> {ResumeStore.description}
-      </p>
+    <div>
+      <div ref={contentRef}>
+        <h2>{ResumeStore.title}</h2>
+        <p>
+          <strong>Name:</strong> {ResumeStore.firstName} {ResumeStore.lastName}
+        </p>
+        <p>
+          <strong>Email:</strong> {ResumeStore.email}
+        </p>
+        <p>
+          <strong>Phone:</strong> {ResumeStore.phoneNumber}
+        </p>
+        <p>
+          <strong>Address:</strong> {ResumeStore.address}
+        </p>
+        <p>
+          <strong>Description:</strong> {ResumeStore.description}
+        </p>
+      </div>
+
       <button onClick={()=>contentSave()}>Save/Print</button>
     </div>
   );
